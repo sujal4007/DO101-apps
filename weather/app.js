6cf7fe17440c092a0895dc11bbd882bbd0af9206
sujal4007 <sujal.bhingarde23@pcu.edu.in>
2025-03-02 22:41:15 +0530

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
-var app = express();
+var app  = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
